1f87d80a86428e81abb1693d0ea64a5bcabdbc10
j0i <z_series@protonmail.ch>
2024-04-10 14:23:21 -0500

    Updated the splash screen

--- app/src/main/java/com/vivokey/sparkactions/presentation/components/SplashScreen.kt
+++ app/src/main/java/com/vivokey/sparkactions/presentation/components/SplashScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -48,8 +49,9 @@ fun SplashScreen(onSplashScreenEnd: () -> Unit) {
             contentAlignment = Alignment.Center
         ) {
             Image(
+                modifier = Modifier.fillMaxWidth(),
                 painter = painterResource(id = R.drawable.splash_screen),
-                contentScale = ContentScale.FillBounds,
+                contentScale = ContentScale.Crop,
                 contentDescription = null
             )
             Column(
